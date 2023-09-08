df281f13f3b658199ed26b20fe6b27be5b3f5be6
Lemon Foxmere <lemon@lemonfoxmere.com>
2023-09-07 20:57:13 -0700

    added frontpage UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+    import MediaQuery from "svelte-media-query";
+</script>
+
+<main>
+    <div id="navbar">
+        <div id="content">
+            
+            <div id="logo">
+                <MediaQuery query="(min-width: 930px)" let:matches>
+                    {#if matches}
+                        <img src="/logo_text.svg" alt="" class="no-drag">
+                    {:else}
+                        <img src="/logo.svg" alt="" class="no-drag">
+                    {/if}
+                </MediaQuery>
+            </div>
+
+            <div id="cta">
+                <MediaQuery query="(min-width: 621px)" let:matches>
+                    {#if matches}
+                        <a href="/login" target="_blank">
+                            <button class="text">Login</button>
+                        </a>
+
+                        <a target="_blank">
+                            <button class="text">
+                                Community
+    
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.75" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>                              
+                            </button>
+                        </a>
+
+                        <a href="https://github.com/LemonFoxmere/LunchRoom" target="_blank">
+                            <button class="text">GitHub</button>
+                        </a>
+                    {:else}
+                        <h3>kys</h3>
+                    {/if}
+                </MediaQuery>
+            </div>
+        </div>
+    </div>
+    <slot></slot>
+</main>
+
+<style lang="scss">
+    @import "$static/stylesheets/guideline";
+
+    main{
+        width: 100%; height: 100%;
+        display: flex; flex-direction: column; justify-content: flex-start; align-items: center;
+        min-height: 100vh;
+
+        #navbar{
+            width: 100%; height: 86px;
+            display: flex; justify-content: center; align-items: flex-end;
+
+            #content{
+                width: calc(100vw - 300px); height: fit-content;
+                display: flex; justify-content: space-between; align-items: center;
+
+                #cta{
+                    display: flex; flex-direction: row-reverse;
+
+                    a{
+                        text-decoration: none;
+
+                        button{
+                            margin-right: 48pt;
+                            display: flex; align-items: center;
+    
+                            svg{
+                                margin-left: 8pt;
+                                height: 14pt;
+                            }
+                        }
+
+                        &:first-child{
+                            margin: 0px;
+                        }
+                    }
+                }
+
+                div{
+                    height: 36px;
+                    display: flex; align-items: center;
+                    overflow: visible;
+                }
+
+                @media screen and (max-width: 930px) {
+                    width: calc(100vw - 100px);
+                }
+            }
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<main>
+    <h1>Fuck urself</h1>
+
+    <section>
+        <button>Sign Up</button>
+        <button class="text">What?</button>
+    </section>
+</main>
+
+<style lang="scss">
+    @import "$static/stylesheets/guideline";
+
+    main{
+        width: 100%; height: 100%;
+        display: flex; justify-content: center; align-items: center; flex-direction: column;
+
+        h1{
+            margin-bottom: 40px;
+        }
+
+        button{
+            width: 180px; height: 50px;
+
+            &.text{
+                text-decoration: underline;
+            }
+        }
+    }
+
+</style>
